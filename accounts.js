9a1c1a5ef231dccd5f24c4c75cbae7abcb9e6557
Ngo Tan Thong <chagtraitroidah@gmail.com>
2026-05-31 21:34:04 +0700

    Cap nhat tai khoan doantrungduc va tieu de trang web

--- accounts.js
+++ accounts.js
@@ -5,4 +5,5 @@ window.ALLOWED_USERS = {
     // "tên_đăng_nhập": "mã_băm_sha256_mật_khẩu"
     "admin": "0883fb27fe5c0a7721a78a406a86d4296b117d0e6527a6b5347996936526de92", // mật khẩu: vnpt2026
     "nhanvien1": "0883fb27fe5c0a7721a78a406a86d4296b117d0e6527a6b5347996936526de92", // mật khẩu: vnpt2026
+    "doantrungduc": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", // mật khẩu: 123456
 };
