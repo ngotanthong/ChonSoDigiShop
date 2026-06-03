d9c590cb976b35789066f769adce9842e4d8e743
Ngo Tan Thong <chagtraitroidah@gmail.com>
2026-06-03 22:06:26 +0700

    Bỏ qua khóa thiết bị cho VIP/Admin

--- accounts.js
+++ accounts.js
@@ -1,6 +1,11 @@
 // accounts.js - Danh sách tài khoản được phép truy cập kho SIM VNPT DigiShop
 // Để thêm tài khoản mới, hãy mở trang taotailoan.html trên trình duyệt để sinh mã cấu hình và dán vào đây!
 
+window.MULTI_DEVICE_USERS = [
+    "admin", // Thêm tên đăng nhập VIP vào đây để cho phép đăng nhập nhiều thiết bị
+    "doantrungduc",
+];
+
 window.ALLOWED_USERS = {
     // "tên_đăng_nhập": "mã_băm_sha256_mật_khẩu"
     "admin": "0883fb27fe5c0a7721a78a406a86d4296b117d0e6527a6b5347996936526de92", // mật khẩu: vnpt2026
